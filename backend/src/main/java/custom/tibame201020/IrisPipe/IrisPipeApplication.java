8c9770602864a92faef017bf498812ef8de82876
Jeff Hsu <tibame201020@gmail.com>
2026-03-06 10:51:44 +0800

    initial backend

---
+++ backend/src/main/java/custom/tibame201020/IrisPipe/IrisPipeApplication.java
@@ -0,0 +1,13 @@
+package custom.tibame201020.IrisPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrisPipeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/custom/tibame201020/IrisPipe/IrisPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package custom.tibame201020.IrisPipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
