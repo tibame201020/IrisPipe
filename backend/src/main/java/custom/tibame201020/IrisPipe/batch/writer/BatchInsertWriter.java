da5443a5d33324e01241477196a7b1b8c40c1cf4
Jeff Hsu <tibame201020@gmail.com>
2026-03-09 10:36:44 +0800

    feat: Add custom Spring Batch writers for inserting and updating data.

--- backend/src/main/java/custom/tibame201020/IrisPipe/batch/writer/BatchInsertWriter.java
+++ backend/src/main/java/custom/tibame201020/IrisPipe/batch/writer/BatchInsertWriter.java
@@ -1,5 +1,33 @@
 package custom.tibame201020.IrisPipe.batch.writer;
 
-public class BatchInsertWriter {
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+
+import custom.tibame201020.IrisPipe.data.SummaryInfo;
+
+public class BatchInsertWriter implements ItemWriter<Map<String, Object>> {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final JdbcBatchItemWriter<Map<String, Object>> writer;
+    private final String destTable;
+    private final SummaryInfo summaryInfo;
+
+    public BatchInsertWriter(JdbcBatchItemWriter<Map<String, Object>> writer, String destTable, SummaryInfo summaryInfo) {
+        writer.afterPropertiesSet();
+        this.writer = writer;
+        this.destTable = destTable;
+        this.summaryInfo = summaryInfo;
+    }
+
+    @Override
+    public void write(Chunk<? extends Map<String, Object>> chunk) throws Exception {
+        writer.write(chunk);
+        summaryInfo.inserted.addAndGet(chunk.size());
+        logger.info("[BatchInsertWriter] processed: {} insert for table: {}", summaryInfo.inserted.get(), destTable);
+    }
 
 }
